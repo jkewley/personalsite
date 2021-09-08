a26f84c6fa014d92e4b3666a4bd871c46bc6f8f0
Josh Kewley <jkewley@insario.com>
2021-09-08 12:16:16 -0500

    header image size tweak

--- src/components/summary.js
+++ src/components/summary.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Summary = ({ data }) => (
   <section className="py-5 my-5 px-10 border-b border-neutral-300 lg:flex items-center justify-start">
     <img
-      className="rounded-full w-48 mx-auto lg:mx-10 xl:w-3/5 xl:min-w-0"
+      className="rounded-full w-48 mx-auto lg:mx-10 xl:w-4/5 xl:min-w-0"
       src="./profile.jpg"
       alt="profile"
     />
