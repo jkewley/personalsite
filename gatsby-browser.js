fbeb323e6e02e212266cc558de0200062d1625bf
Josh Kewley <jkewley@insario.com>
2021-09-15 22:12:09 -0500

    just reload when update available. No need to prompt

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,11 @@
 export const onServiceWorkerUpdateReady = () => {
   // eslint-disable-next-line no-alert
-  const answer = window.confirm(
-    'This application has been updated. Reload to display the latest version?'
-  );
-  if (answer === true) {
+
+  // just reload when update available. No need to prompt
+  // const answer = window.confirm(
+  //   'This application has been updated. Reload to display the latest version?'
+  // );
+  // if (answer === true) {
     window.location.reload();
-  }
+  // }
 };
